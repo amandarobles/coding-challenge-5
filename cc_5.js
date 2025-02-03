7d6cfc9cda328702f8abf8bf08bedc6b2426669b
Amanda Robles <amandarobles@usf.edu>
2025-02-03 11:28:29 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,13 @@
+// task 1: Customer Profile
+
+//Declaring object customers with properties
+let customer = {
+    name: "John Doe"
+    age: 35,
+    email: "john.doe@email.com"
+};
+
+//Logging properties
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
